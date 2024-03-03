9ce2a30a4f2f582cdbf88e79f18d71b5f1134afe
ahme2001 <ahmedaboeleid30@gmail.com>
2024-03-03 20:13:53 +0200

    ..

--- eagle_submission_solver.py
+++ eagle_submission_solver.py
@@ -1,6 +1,8 @@
 import numpy as np
 import LSBSteg
 import requests
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 # api_base_url = 'http://3.70.97.142:5000'
 # team_id= 'BFhxJPg'
@@ -25,16 +27,16 @@ def init_eagle(team_id):
     payload = {
         'teamId': team_id
     }
-    response = requests.post(api_base_url+"/eagle/start", json=payload)
+    response = requests.post(api_base_url+"/eagle/start", json=payload , headers={'Content-Type': 'application/json'})
     if response.status_code == 200 or response.status_code == 201:
         try:
             data = response.json()
-            print(data)
+
             # Extract footprints
             footprint1 = np.array(data['footprint']['1'])
             footprint2 = np.array(data['footprint']['2'])
             footprint3 = np.array(data['footprint']['3'])
-
+            print("footprint1 shape: " , footprint1.shape)
             return footprint1 , footprint2 , footprint3
         except Exception as e:
             print("Error parsing response in init function :", e)
@@ -44,6 +46,108 @@ def init_eagle(team_id):
         print("Init Error:", response.status_code)
         end_eagle()
         return None
+
+def request_msg(team_id, channel_id):
+    '''
+    If you decide to listen to any of the 3 channels then you need to hit the end point of selecting a channel to hear on (1,2 or 3)
+    '''
+    payload = {
+        'teamId': team_id , 
+        'channelId': channel_id
+    }
+    response = requests.post(api_base_url+"/eagle/request-message", json=payload , headers={'Content-Type': 'application/json'})
+    if response.status_code == 200 or response.status_code == 201:
+        try:
+            data = response.json()
+            encodedMsg = np.array(data['encodedMsg'])
+            return encodedMsg
+        except Exception as e:
+            print("Error parsing response in request function :", e)
+            end_eagle()
+            return None
+    else:
+        print("Error in request function:", response.status_code)
+        end_eagle()
+        return None
+
+def skip_msg(team_id):
+    '''
+    If you decide to NOT listen to ANY of the 3 channels then you need to hit the end point skipping the message.
+    If sucessful request to the end point , you will expect to have back new footprints IF ANY.
+    '''
+    payload = {
+        'teamId': team_id
+    }
+    response = requests.post(api_base_url+"/eagle/skip-message", json=payload , headers={'Content-Type': 'application/json'})
+    if response.status_code == 200 or response.status_code == 201:
+        try:
+            data = response.json()
+
+            footprint1 = np.array(data['nextFootprint']['1'])
+            footprint2 = np.array(data['nextFootprint']['2'])
+            footprint3 = np.array(data['nextFootprint']['3'])
+
+            return footprint1 , footprint2 , footprint3
+        except Exception as e:
+            print("data " , data)
+            print("Message is finished or error parsing response in submit function :", e)
+            end_eagle()
+            return None
+    else:
+        print("Error in Skipping function:", response.status_code)
+        end_eagle()
+        return None
+
+def submit_msg(team_id, decoded_msg):
+    '''
+    In this function you are expected to:
+        1. Decode the message you requested previously
+        2. call the api end point to send your decoded message  
+    If sucessful request to the end point , you will expect to have back new footprints IF ANY.
+    '''
+    payload = {
+        'teamId': team_id , 
+        'decodedMsg': decoded_msg
+    }
+    response = requests.post(api_base_url+"/eagle/submit-message", json=payload , headers={'Content-Type': 'application/json'})
+    if response.status_code == 200 or response.status_code == 201:
+        try:
+            data = response.json()
+
+            footprint1 = np.array(data['nextFootprint']['1'])
+            footprint2 = np.array(data['nextFootprint']['2'])
+            footprint3 = np.array(data['nextFootprint']['3'])
+
+            return footprint1 , footprint2 , footprint3
+        except Exception as e:
+            print("data " , data)
+            print("Message is finished or error parsing response in submit function :", e)
+            end_eagle()
+            return None
+    else:
+        # Print an error message if the request was not successful
+        print("Error in submit function:", response.status_code)
+        end_eagle()
+        return None
+
+def end_eagle(team_id):
+    '''
+    Use this function to call the api end point of ending the eagle  game.
+    Note that:
+    1. Not calling this fucntion will cost you in the scoring function
+    '''
+    payload = {
+        'teamId': team_id
+    }
+    response = requests.post(api_base_url+"/eagle/end-game", json=payload , headers={'Content-Type': 'application/json'})
+    if response.status_code == 200 or response.status_code == 201:
+        try:
+            data = response.json()
+            print("end function " , data)
+        except Exception as e:
+            print("Error parsing response in end function :", e)
+    else:
+        print("End error:", response.status_code)
 
 def select_channel(footprint): # return number of selected channel
     '''
@@ -65,112 +169,6 @@ def select_channel(footprint): # return number of selected channel
         return 3
     else:
         return 0
-
-def skip_msg(team_id):
-    '''
-    If you decide to NOT listen to ANY of the 3 channels then you need to hit the end point skipping the message.
-    If sucessful request to the end point , you will expect to have back new footprints IF ANY.
-    '''
-    payload = {
-        'teamId': team_id
-    }
-    response = requests.post(api_base_url+"/eagle/skip-message", json=payload)
-    if response.status_code == 200 or response.status_code == 201:
-        try:
-            data = response.json()
-            print(data)
-            # Extract footprints
-            footprint1 = np.array(data['nextFootprint']['1'])
-            footprint2 = np.array(data['nextFootprint']['2'])
-            footprint3 = np.array(data['nextFootprint']['3'])
-
-            return footprint1 , footprint2 , footprint3
-        except Exception as e:
-            print("Message is finished or error parsing response in submit function :", e)
-            end_eagle()
-            return None
-    else:
-        # Print an error message if the request was not successful
-        print("Error in Skipping function:", response.status_code)
-        end_eagle()
-        return None
-
-def request_msg(team_id, channel_id):
-    '''
-    If you decide to listen to any of the 3 channels then you need to hit the end point of selecting a channel to hear on (1,2 or 3)
-    '''
-    payload = {
-        'teamId': team_id , 
-        'channelId': channel_id
-    }
-    response = requests.post(api_base_url+"/eagle/request-message", json=payload)
-    if response.status_code == 200 or response.status_code == 201:
-        try:
-            data = response.json()
-            encodedMsg = np.array(data['encodedMsg'])
-            return encodedMsg
-        except Exception as e:
-            end_eagle()
-            print("Error parsing response in request function :", e)
-            return None
-    else:
-        print("Error in request function:", response.status_code)
-        end_eagle()
-        return None
-
-def submit_msg(team_id, decoded_msg):
-    '''
-    In this function you are expected to:
-        1. Decode the message you requested previously
-        2. call the api end point to send your decoded message  
-    If sucessful request to the end point , you will expect to have back new footprints IF ANY.
-    '''
-    payload = {
-        'teamId': team_id , 
-        'decodedMsg': decoded_msg
-    }
-    response = requests.post(api_base_url+"/eagle/submit-message", json=payload)
-    if response.status_code == 200 or response.status_code == 201:
-        try:
-            data = response.json()
-            print(data)
-            # Extract footprints
-            footprint1 = np.array(data['nextFootprint']['1'])
-            footprint2 = np.array(data['nextFootprint']['2'])
-            footprint3 = np.array(data['nextFootprint']['3'])
-
-            return footprint1 , footprint2 , footprint3
-        except Exception as e:
-            print("Message is finished or error parsing response in submit function :", e)
-            end_eagle()
-            return None
-    else:
-        # Print an error message if the request was not successful
-        print("Error in submit function:", response.status_code)
-        end_eagle()
-        return None
-
-def end_eagle(team_id):
-    '''
-    Use this function to call the api end point of ending the eagle  game.
-    Note that:
-    1. Not calling this fucntion will cost you in the scoring function
-    '''
-    payload = {
-        'teamId': team_id
-    }
-    response = requests.post(api_base_url+"/eagle/end-game", json=payload)
-    if response.status_code == 200 or response.status_code == 201:
-        try:
-            data = response.json()
-            print(data)
-        except Exception as e:
-            print("Error parsing response in end function :", e)
-            end_eagle()
-    else:
-        # Print an error message if the request was not successful
-        print("End error:", response.status_code)
-        end_eagle()
 
 def submit_eagle_attempt(team_id):
     '''
@@ -203,6 +201,21 @@ def submit_eagle_attempt(team_id):
                     return None
     else:
         print("Error initializing the game")
+
+def start_model():
+    loaded_model = Sequential([
+        Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(1998, 101, 1)),
+        MaxPooling2D(pool_size=(2, 2)),
+        Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+    loaded_model.load_weights('model_weights.h5')
+    return loaded_model
+
 
 
 submit_eagle_attempt(team_id)
